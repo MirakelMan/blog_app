b47c472efad7156628ba662d9f45b6b5980294f2
MirakelMan <pascal.vdpol@gmail.com>
2023-09-03 13:14:30 +0200

    add gmail account

--- mirakelblog/mirakelblog/settings.py
+++ mirakelblog/mirakelblog/settings.py
@@ -122,3 +122,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Email server configuration
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "rheinhardtdjango@gmail.com"
+EMAIL_HOST_PASSWORD = "onnbvlefkpamgbov"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
