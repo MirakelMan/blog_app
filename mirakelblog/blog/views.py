e23b1e4cfe4110040b995a6b0a80d9b5483def22
MirakelMan <pascal.vdpol@gmail.com>
2023-09-04 16:29:46 +0200

    add basic search engine

--- mirakelblog/blog/views.py
+++ mirakelblog/blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.postgres.search import SearchVector
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db.models import Count
@@ -9,7 +10,7 @@ from taggit.models import Tag
 
 from mirakelblog.settings import EMAIL_HOST_USER
 
-from .forms import CommentForm, EmailPostForm
+from .forms import CommentForm, EmailPostForm, SearchForm
 from .models import Comment, Post
 
 
@@ -110,4 +111,22 @@ def post_comment(request: HttpRequest, id: int):
         request,
         "blog/post/comment.html",
         {"post": post, "form": form, "comment": comment},
+    )
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if "query" in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data["query"]
+            results = Post.published.annotate(
+                search=SearchVector("title", "body"),
+            ).filter(search=query)
+    return render(
+        request,
+        "blog/post/search.html",
+        {"form": form, "query": query, "results": results},
     )
